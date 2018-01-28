906de2d15a83e39a628cf784639de3f37ec87a39
Allison Bolen <bolena@mail.gvsu.edu>
2018-01-28 15:50:42 -0500

    testing fetches

---
+++ library.h
@@ -0,0 +1,8 @@
+#ifndef			__H_MY_LIBRARY__
+#define			__H_MY_LIBRARY__
+
+// As this is an *.h file, this is an interface.  No
+// code is provided for how the function should work.
+void handyFunction(int num);
+
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// This is a library we created, of code we may wish to use
+// in multiple projects.
+#include "library.h"
+
+// This is the program's entry point
+int main(int argc, char** argv){
+	printf("Howdy world!\n");
+	handyFunction(42);
+}
