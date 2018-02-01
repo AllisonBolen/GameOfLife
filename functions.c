4babaeb4b98922d7a1c2f1abc3701b77eb02dc79
glynnja <jacksonglynn@yahoo.com>
2018-01-31 22:00:46 -0500

    Created building blocks

---
+++ functions.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int read_file( char* filename, char **buffer ){
+  FILE* in = fopen(filename, "r");
+
+  
+  if (in == NULL){
+    printf ("file could not be opened!\n");
+    exit(1);
+  }
+  else{
+    printf("Please enter the number of rows.\n");
+    int row;
+    scanf(" %d", &row);
+    printf("Please enter the number of columns.\n");
+    int column;
+    scanf(" %d", &column);
+    *buffer = malloc(row * column * sizeof(char));
+    printf("%d%d\n", row,column);
+    
+
+    return 0;
+  }
+}
+
+
+int write_file( char* filename, char *buffer, int size){
+  FILE* out = fopen(filename, "w");
+  
+  
+}
+
---
+++ life.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <library.h>
+
+int main(int argc, int** argv[]){
+  printf("%s\n", argv[1]);
+  char* buffer;
+  //buffer = malloc(500 * sizeof(char));
+  read_file( argv[1], &buffer );
+   
+  printf ("Would you like to save the file type Y for yes or N for no.");
+  //scanf ("%s", userprompt);
+  //if (userprompt == "Y"){
+ //   int write_file( char* filename, char *buffer, int size);
+ // }
+  printf(" It didnt work");
+}
+
+
