5a04607a115f309986fcbb4e41a0e8ec808ab7ad
Allison Bolen <bolena@mail.gvsu.edu>
2018-02-07 18:26:07 -0500

    this

--- life.c
+++ life.c
@@ -5,11 +5,13 @@
 int main(int argc, int** argv[]){
   printf("%s\n", argv[1]);
   char* buffer;
+  char* board;
   //buffer = malloc(500 * sizeof(char));
   read_file( argv[1], &buffer );
   printf("%s", buffer);
-
+  int* r = 4;
+  int* c = 4;
   printf ("\nWould you like to save the file type Y for yes or N for no.\n");
-  makeBoard();
+  makeBoard(&r, &c, &board, &buffer));
   printf("\n It didnt work");
 }
