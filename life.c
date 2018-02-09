6f737ea4a51ee4b9569d79f2f17bbd966b6faf0e
glynnja <jacksonglynn@yahoo.com>
2018-02-08 21:22:04 -0500

    Cleaned main

--- life.c
+++ life.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <library.h>
 
 int main(int argc, int** argv[]){
   printf("%s\n", argv[1]);
   char* buffer;
   char** board;
-  //buffer = malloc(500 * sizeof(char));
   int size = read_file( argv[1], &buffer );
   printf("%s", buffer);
   int r,c,i,j; 
@@ -19,26 +17,7 @@ int main(int argc, int** argv[]){
   printf ("\nMakeBoard:\n");
   makeBoard(r, c, &board, buffer);
   printf("\n It didnt work\n ");
-  
- /*for (i = 0; i < strlen(buffer); i++){
-	if(buffer[i] == '\n'){
-		//printf("this is a new line \n");
-	}
-	else{
-	printf("%c", buffer[i]);
-	}	
-   
-  }*/
-	printf("\n");
+  printf("\n");
   return 0;
 
-
- /* 
-  int r = 4;
-  int c = 4;
-  printf ("\nWould you like to save the file type Y for yes or N for no.\n");
-  makeBoard(r, c, &board, buffer);
-  printf("\n It didnt work");
-  return 0;
- */ 
 }
