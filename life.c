38c0986417ce12217e12f78a78a0249bde9c6db0
glynnja <jacksonglynn@yahoo.com>
2018-02-08 22:39:37 -0500

    Added comments that were deleted

--- life.c
+++ life.c
@@ -11,4 +11,32 @@ int main(int argc, int** argv[]){
   loop(&r,&c);
   makeBoard(&r,&c, &board, buffer);
   return 0;
+
+
+
+
+  while(response != 'q'){
+    // calc board?
+    // after each check ask user what they want
+
+    // if resp = s
+    // write to file
+    // ask again?
+
+    // if resp load
+    // free all boards and buffers
+    // read from filename
+    // ask for loop nums?
+
+    // if resp = c
+    // run once for a new single generation
+    // ask for what to do next
+ 
+    // if reps = cn
+    // loop for that many generations
+    // ask user what to do next
+  }
+    // if resp = q
+    // free baords and buffers
+    // system exit with 0
 }
